5acd6eaa40909654f95489e03c11acf7d79eb7b8
irosen419 <irosen419@gmail.com>
2020-09-06 01:12:50 -0400

    same issues as before

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,18 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find_by(id: params[:id])
+        user = User.find(params[:id])
         render json: user
     end
     
     def create
-        User.create(user_params(:username))
+        user = User.create(user_params(:username))
+        render json: user
     end
 
     def update
-        user = User.find_by(id: params[:id])
+        user = User.find(params[:id])
+        # byebug
         user.update(user_params(:highscore))
     end
 
