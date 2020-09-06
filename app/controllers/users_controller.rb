c632934876680e3e319e9a1d2d42b0d38e47ca9b
irosen419 <irosen419@gmail.com>
2020-09-05 20:45:57 -0400

    made create and update controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,19 @@ class UsersController < ApplicationController
         user = User.find_by(id: params[:id])
         render json: user
     end
+    
+    def create
+        User.create(user_params(:username))
+    end
 
     def update
         user = User.find_by(id: params[:id])
-        user.update(user_params)
+        user.update(user_params(:highscore))
     end
 
     private
 
-    def user_params
-        params.require(:user).permit(:highscore)
+    def user_params(*args)
+        params.require(:user).permit(*args)
     end
 end
